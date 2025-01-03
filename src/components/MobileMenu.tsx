ee8437d3afb659fbf7715729aa164f389c03a9a4
emmanuelcodechimp <emmanuel@codechimpit.com>
2025-01-03 16:54:02 +0000

    working on navbar

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -8,23 +8,23 @@ const MobileMenu = () => {
     <div>
       {/* humburger-icon */}
       <div
-        className="flex flex-col gap-[4.5px] cursor-pointer"
+        className="flex md:hidden flex-col gap-[4.5px] cursor-pointer"
         onClick={() => setIsOpen((prev) => !prev)}
       >
         <div
           className={`w-6 bg-blue-500 rounded-sm h-1 ${
-            isOpen ? "rotate-45 origin-left" : ""
+            isOpen ? "rotate-45 origin-left ease-in-out duration-500" : ""
           } `}
         ></div>
         <div
           className={`w-6 bg-blue-500 rounded-sm h-1 ${
             isOpen ? "opacity-0" : ""
-          }`}
+          }  ease-in-out duration-500`}
         ></div>
         <div
           className={`w-6 bg-blue-500 rounded-sm h-1 ${
-            isOpen ? "-rotate-45 origin-left" : ""
-          }`}
+            isOpen ? "-rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
         ></div>
       </div>
       {/* mobile menu */}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,55 @@
 import React from 'react'
 import MobileMenu from './MobileMenu';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
       <div className="flex items-center justify-between h-24">
         {/* // left */}
-        <div className="">
+        <div className="md:hidden lg:block w-[20%]">
           <Link href="/" className="font-bold text-2xl text-blue-600">
             <span>edyXcode</span>
           </Link>
         </div>
         {/* // middle */}
-        <div className="hidden"></div>
+        <div className="hidden md:flex text-sm w-[50%]">
+          {/* links */}
+          <div className="flex gap-6 text-gray-600">
+            <Link href="/" className="flex items-center gap-2">
+              <Image
+                src="/home.png"
+                alt="home"
+                width={16}
+                height={16}
+                className="w-4 h-4"
+              />
+              <span>HomePage</span>
+            </Link>
+            <Link href="/" className="flex items-center gap-2">
+              <Image
+                src="/friends.png"
+                alt="home"
+                width={16}
+                height={16}
+                className="w-4 h-4"
+              />
+              <span>Friends</span>
+            </Link>
+            <Link href="/" className="flex items-center gap-2">
+              <Image
+                src="/stories.png"
+                alt="home"
+                width={16}
+                height={16}
+                className="w-4 h-4"
+              />
+              <span>Stories</span>
+            </Link>
+          </div>
+        </div>
         {/* // right */}
-        <div className="">
+        <div className="w-[30%] flex items-center justify-end gap-4 xl:gap-8">
           <MobileMenu />
         </div>
       </div>
