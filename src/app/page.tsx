2d4e202eecf241e6edee09ff73af41eec5020add
emmanuelcodechimp <emmanuel@codechimpit.com>
2025-01-03 19:13:49 +0000

    working on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
+import AddPost from "@/components/AddPost";
+import Feed from "@/components/Feed";
+import LeftMenu from "@/components/LeftMenu";
+import RightMenu from "@/components/RightMenu";
+import Stories from "@/components/Stories";
 
 export default function Home() {
   return (
-    <div className="">
-      home
+    <div className="flex gap-6 pt-6">
+      {/* left */}
+      <div className="hidden xl:block xl:w-[20%]">
+      <LeftMenu/> 
+      </div>
+      {/* middle */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed/>
+        </div>
+      </div>
+      {/* right */}
+      <div className="hidden lg:w-[30%] lg:block">
+      <RightMenu/>
+      </div>
    </div>
   );
 }
---
+++ src/components/RightMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RightMenu = () => {
+  return (
+    <div>
+          right-menu
+          
+
+          fk;
+    </div>
+  )
+}
+
+export default RightMenu
